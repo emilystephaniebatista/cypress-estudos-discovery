cbcb3bbea985a96a37fd78af3e15bcbfcd934ddb
WBJ-000337 <emily.batista@webjump.com.br>
2022-10-20 11:32:25 -0300

    Criando PageObjects

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,10 +1,7 @@
+import SignupPage from '../pages/SignupPage'
+
 describe('Cadastro', ()=>{
     it('Usuário deve se tornar um entregador', ()=>{
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
-        
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
         var deliver = {
             name: 'Emily Stephanie',
@@ -22,39 +19,17 @@ describe('Cadastro', ()=>{
             },
             delivery_method: 'Moto',
             cnh: 'cnh-digital.jpg'
-
         }
 
-        //preenchendo os valores dos campos-clicando no botão de busca de cep
-        cy.get('input[name="name"]').type(deliver.name)
-        cy.get('input[name="cpf"]').type(deliver.cpf)
-        cy.get('input[name="email"]').type(deliver.email)
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
-        cy.get('input[type=button][value="Buscar CEP"]').click()
-        cy.get('input[name="address-number"]').type(deliver.address.number)
-        cy.get('input[name="address-details"]').type(deliver.address.details)
-
-        //validando os campos que foram preenchidos do endereço
-        cy.get('input[name="address"]').should('have.value', deliver.address.street)
-        cy.get('input[name="district"]').should('have.value', deliver.address.district)
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-
-        //Para ir o valor da moto direto no método da entrega tem que inserir em: metodo_entrega
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-        //Para fazer upload de imagens
-        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
-        
-        //clicar no botão
-        cy.get('form button[type="submit"]').click()
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-        
-        cy.get('.swal2-container .swal2-html-container')
-            .should('have.text', expectedMessage)
-    
+        signup.modalContentShouldBe(expectedMessage)        
+
     })
 })
 
@@ -83,31 +58,10 @@ it('CPF incorreto', ()=>{
         cnh: 'cnh-digital.jpg'
 
     }
-     //preenchendo os valores dos campos-clicando no botão de busca de cep
-     cy.get('input[name="name"]').type(deliver.name)
-     cy.get('input[name="cpf"]').type(deliver.cpf)
-     cy.get('input[name="email"]').type(deliver.email)
-     cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+    var signup = new SignupPage()
 
-     cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
-     cy.get('input[type=button][value="Buscar CEP"]').click()
-     cy.get('input[name="address-number"]').type(deliver.address.number)
-     cy.get('input[name="address-details"]').type(deliver.address.details)
-
-     //validando os campos que foram preenchidos do endereço
-     cy.get('input[name="address"]').should('have.value', deliver.address.street)
-     cy.get('input[name="district"]').should('have.value', deliver.address.district)
-     cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-
-     //Para ir o valor da moto direto no método da entrega tem que inserir em: metodo_entrega
-     cy.contains('.delivery-method li', deliver.delivery_method).click()
-     
-     //Para fazer upload de imagens
-     cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
-     
-     //clicar no botão
-     cy.get('form button[type="submit"]').click()
-
-     cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
-
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! CPF inválido')
 })
---
+++ cypress/pages/SignupPage.js
@@ -0,0 +1,53 @@
+class SignupPage{
+    go(){
+
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+        
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+    }
+
+    fillForm(deliver){
+        //preenchendo os valores dos campos-clicando no botão de busca de cep
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
+
+        //validando os campos que foram preenchidos do endereço
+        cy.get('input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
+
+        //Para ir o valor da moto direto no método da entrega tem que inserir em: metodo_entrega
+        cy.contains('.delivery-method li', deliver.delivery_method).click()
+
+        //Para fazer upload de imagens
+        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
+                
+    }
+
+    submit(){
+
+        //clicar no botão
+        cy.get('form button[type="submit"]').click()
+    }
+
+    modalContentShouldBe(expectedMessage){
+        cy.get('.swal2-container .swal2-html-container')
+        .should('have.text', expectedMessage)
+    }
+    alertMessageShouldBe(expectedMessage){
+
+        cy.get('.alert-error').should('have.text', expectedMessage)
+
+    }
+}
+export default SignupPage;
