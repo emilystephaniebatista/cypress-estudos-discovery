c80fe101d333d76c7c46c674fdb5cc24d85a7558
WBJ-000337 <emily.batista@webjump.com.br>
2022-10-26 11:11:18 -0300

    estudos

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,5 +1,6 @@
 import signup from '../pages/SignupPage'
 import signupFactory from '../factories/SignupFactory'
+import signupPage from '../pages/SignupPage'
 
 describe('Signup', ()=>{
 
@@ -55,5 +56,38 @@ describe('Signup', ()=>{
             signup.submit()
             signup.alertMessageShouldBe('Oops! Email com formato inválido.')
     })
+    //para continuar testando sem pausar o teste mesmo se der erro na loja
+
+    context('Required fields', function(){
+        const messages = [
+            {field:'name', output: 'É necessário informar o nome'},
+            {field:'cpf', output: 'É necessário informar o CPF'},
+            {field:'email', output: 'É necessário informar o email'},
+            {field:'postalcode', output: 'É necessário informar o CEP'},
+            {field:'number', output: 'É necessário informar o número do endereço'},
+            {field:'delivery_method', output: 'Selecione o método de entrega'},
+            {field:'cnh', output: 'Adicione uma foto da sua CNH'}
+        ]
+        before(function(){
+            signupPage.go()
+            signupPage.submit()
+        })
+        messages.forEach(function(msg){
+            it(`${msg.field} is required`, function(){
+                signupPage.alertMessageShouldBe(msg.output)
+            })
+        })
+    })
+    //it('Required fields', function(){
+    //    signupPage.go()
+    //    signupPage.submit()
+    //    signupPage.alertMessageShouldBe('É necessário informar o nome')
+    //    signupPage.alertMessageShouldBe('É necessário informar o CPF')
+    //    signupPage.alertMessageShouldBe('É necessário informar o email')
+    //    signupPage.alertMessageShouldBe('É necessário informar o CEP')
+    //    signupPage.alertMessageShouldBe('É necessário informar o número do endereço')
+    //    signupPage.alertMessageShouldBe('Selecione o método de entrega')
+    //    signupPage.alertMessageShouldBe('Adicione uma foto da sua CNH')
+    //})
 })
 
